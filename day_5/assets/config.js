ed8a7c30e3fc36d64181d39418b463e0025bf413
oumaima <omaimaelbdraouy@gmail.com>
2025-09-06 21:07:11 +0100

    Initial commit

---
+++ day_5/assets/config.js
@@ -0,0 +1,2 @@
+export const BASE_URL ="https://script.google.com/macros/s/AKfycbwun8k0MdxepMD_59RKU-jDYizLKLjxwqa84CrVSW6b8Xlc6jwQ4TmYn4aNnuNymr0bdQ/exec"
+export const SECRET_KEY = "geeks-secret";
